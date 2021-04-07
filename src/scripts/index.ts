5115c6eb940a611f1856737598638be3673ab645
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-04-07 14:51:32 +0200

    Add testing data to fill forms

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,4 +1,5 @@
 import type { Participant } from '../@types/participant';
+import { fillForm } from '../helpers/forms';
 import Store from '../helpers/store';
 
 const data: Participant[] = [
@@ -27,6 +28,17 @@ const data: Participant[] = [
 	}
 
 	const result = await participants.getItems<Participant>({ surname: 'doe' });
-
 	console.log(result);
+
+	const form = document.getElementById('form');
+	if (form)
+		fillForm<Participant & { id: number; payed: boolean; time: number }>(form as HTMLFormElement, {
+			id: 2,
+			time: 34200,
+			payed: true,
+			name: 'Jane',
+			surname: 'DOE',
+			phone: '+123456789',
+			email: 'jane@doe.com'
+		});
 })();
