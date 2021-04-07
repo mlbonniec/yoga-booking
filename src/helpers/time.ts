6508341f5dc2ffaeb0d9460dfc1163b52875da62
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-04-07 18:53:10 +0200

    Remove default value

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,11 +1,11 @@
-import { duration, utc } from "moment";
+import { duration, utc } from 'moment';
 
 export function toSeconds(hour:string): number {
-    if(!hour) return 0
+    if(!hour) return 0;
     return duration(hour).asSeconds();
 }
 
 export function toHour(seconds:number): string{
-    if(!seconds) return "00:00"
+    if(!seconds) return '00:00';
     return utc(seconds * 1000).format('HH:mm');
 }
