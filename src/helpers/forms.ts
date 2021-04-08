d3290abcdc6504bc9de8a2721f21146cfe0a3dbd
Iziram <matthiworld@gmail.com>
2021-04-08 10:27:01 +0200

    Update forms.ts

--- src/helpers/forms.ts
+++ src/helpers/forms.ts
@@ -1,4 +1,6 @@
-import { toHour } from './time';
+import {toHour} from './time';
+import type { Participant, Room, Speaker, Workshop } from '../@types/structures';
+import Store from '../helpers/store';
 
 export function fillForm<T extends { [key: string]: any }>(el: HTMLFormElement, data: T) {
 	if (!el)
@@ -24,4 +26,40 @@ export function fillForm<T extends { [key: string]: any }>(el: HTMLFormElement, 
 				break;
 		}
 	});
+}
+
+
+export function getForms(form:HTMLFormElement): object{
+	const inputs = Array.from(form.getElementsByTagName('input'));
+	let ob = {
+
+	}
+	inputs.forEach(element => {
+		if(element.getAttribute('name')){
+			ob[element.getAttribute('name')] = element.value
+		}
+		
+	});
+	return ob;
+}
+
+
+export function addToDb(ob:object){
+	if(ob["payed"] != undefined){
+		ob = ob as Participant;
+		if(ob["id"] !== undefined){
+			let id = ob["id"]
+			delete(ob["id"]);
+			
+			(async() =>{
+				const participants = new Store("participants")
+				// await participants.setItem<Participant>(obj)
+			})();
+		}else{
+			(async() =>{
+				const participants = new Store("participants")
+				// await participants.setItem<Participant>(obj)
+			})();
+		}
+	}
 }
