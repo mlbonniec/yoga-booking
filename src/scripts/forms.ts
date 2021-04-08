c3420d8af16cbcc55cd4a1e2eed2b94e9245df66
Joonnekindt Brieuc <onwix85@gmail.com>
2021-04-08 11:42:15 +0200

    Ajout de Blockage de l'envoie des formulaires

---
+++ src/scripts/forms.ts
@@ -0,0 +1,6 @@
+const form = document.getElementById('test')
+console.log(form)
+form?.addEventListener("submit", function(event){
+    event.preventDefault()
+    //+Ajouter les fonctionalité de sauvegarde
+})
