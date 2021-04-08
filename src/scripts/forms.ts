32bb67c502b41f6c449e187f14e9c6669a5fd3ba
Joonnekindt Brieuc <onwix85@gmail.com>
2021-04-08 12:26:02 +0200

    Recuperer un identifiant dans l'url

--- src/scripts/forms.ts
+++ src/scripts/forms.ts
@@ -1,5 +1,5 @@
 const form = document.getElementById('test')
-console.log(form)
+
 form?.addEventListener("submit", function(event){
     event.preventDefault()
     //+Ajouter les fonctionalité de sauvegarde
