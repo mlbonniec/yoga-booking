7de1e01af59545629f3a78e464e5bf39e3701964
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-04-26 10:19:10 +0200

    Display success notification on form send

--- src/scripts/forms.ts
+++ src/scripts/forms.ts
@@ -6,6 +6,6 @@ form?.addEventListener('submit', function(e) {
     e.preventDefault();
     
     // + Ajouter les fonctionnalités de sauvegarde
-    // success('Modification effectuée avec succès.');
-    // error('Une erreur est survenue.')
+    success('Modification effectuée avec succès.');
+    // error('Une erreur est survenue.', { confirmButtonText: 'Fermer' });
 });
