a59e37e429bb9c271e10c14f57a5d7a706ae8191
Elouan Murat <elouan.murat@live.fr github>
2021-04-29 11:45:32 +0200

    Fonction Conflit

--- src/scripts/Conflit.ts
+++ src/scripts/Conflit.ts
@@ -1,12 +1,22 @@
 import type { Participant, Room, Speaker, Workshop } from '../@types/structures';
 import Store from '../helpers/store';
 
-function conflit(id, atelier, horaire){
-    (async () => {
+async function conflit(id:number, debut:number, fin:number){
+
         const tableP= new Store('Participant');
         const tableW = new Store('Workshop');
-        const resultP = await tableP.getItems({ id: workshop });
+        const ResultW = await tableP.getItem<Participant>(id);
         //{ clé: valeur } = condition
-        }();
-    )
+        if (ResultW == null){ return false }
+        for (const element in ResultW.workshops) {
+            const workshop = await tableW.getItem<Workshop>(element)
+            if (workshop != null){
+                const debutW = workshop.start
+                const finW = workshop.end
+                if ((debut >= debutW || debut <= finW) || (fin >= debutW || fin <= finW)){
+                    return true
+            }
+            }
+        }
+        return false
 }
