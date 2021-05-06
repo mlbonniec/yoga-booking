08e0d8a1de73bc6ad7f4dcebd7e07140741f97df
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-05-06 10:30:04 +0200

    Ajout de la documentation de forms.ts

--- src/helpers/forms.ts
+++ src/helpers/forms.ts
@@ -1,6 +1,13 @@
 import { toHour, toSeconds } from './time';
 import Store from '../helpers/store';
 
+/**
+ * @description Fill a given form with given data.
+ * @example <caption>Suppose the form has 2 text fields 'name' and 'email', and a 'payed' checkbox.</caption>
+ * const form = document.getElementById('myform');
+ * const data = { name: 'DOE', email: 'john.doe@domain.com', payed: true };
+ * fillForm(form, data);
+ */
 export function fillForm<T extends { [key: string]: any }>(el: HTMLFormElement, data: T) {
 	if (!el)
 		throw new Error('You have to pass a form element in the fillForm function.');
@@ -27,7 +34,13 @@ export function fillForm<T extends { [key: string]: any }>(el: HTMLFormElement, 
 	});
 }
 
-export function getForm(form: HTMLFormElement): object {
+/**
+ * @description Get and parses data from a given form. Returned keys correspond to the 'name' attributes of the inputs.
+ * @example <caption>Suppose the form has 2 text fields 'name' and 'email', and a 'payed' checkbox.</caption>
+ * const form = document.getElementById('myform');
+ * getFormData(form); // { name: 'DOE', email: 'john.doe@domain.com', payed: true };
+ */
+export function getFormData(form: HTMLFormElement): object {
 	const inputs = Array.from(form.getElementsByTagName('input'));
 	const data: { [key: string]: any } = {};
 
@@ -52,6 +65,12 @@ export function getForm(form: HTMLFormElement): object {
 	return data;
 }
 
+/**
+ * @description Add data to the storage. It automatically remove the 'id' key from data.
+ * @example
+ * const data = { name: 'DOE', surname: 'John', email: 'john.doe@domain.com', phone: '+33636291634' };
+ * addToDB('speakers', data); // { name: 'DOE', surname: 'John', email: 'john.doe@domain.com', phone: '+33636291634' }
+ */
 export async function addToDB<T extends { id?: number }>(structure: string, data: T): Promise<T> {
 	const id = data.id;
 	const store = new Store(structure);
@@ -67,6 +86,6 @@ export async function addToDB<T extends { id?: number }>(structure: string, data
 // const tonElement = document.getElementById('bouton') as HTMLButtonElement;
 // tonElement.addEventListener('click', () => {
 // 	const form = document.getElementById("form") as HTMLFormElement;
-// 	addToDb("participants", getForm(form))
+// 	addToDb("participants", getFormData(form))
 // 	document.location.reload()
 //   });
