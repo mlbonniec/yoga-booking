6af92c97928d71488c4e85867a86baa1e9806b19
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-05-06 10:17:58 +0200

    Ajout de la documentation de time.ts

--- src/helpers/time.ts
+++ src/helpers/time.ts
@@ -1,11 +1,21 @@
 import { duration, utc } from 'moment';
 
-export function toSeconds(hour:string): number {
+/**
+ * @description Convert string hour to seconds
+ * @example
+ * toSeconds('12:30'); // 43200
+ */
+export function toSeconds(hour: string): number {
     if(!hour) return 0;
     return duration(hour).asSeconds();
 }
 
-export function toHour(seconds:number): string{
+/**
+ * @description Convert seconds to string hour
+ * @example
+ * toSeconds('43200'); // '12:30'
+ */
+export function toHour(seconds: number): string {
     if(!seconds) return '00:00';
     return utc(seconds * 1000).format('HH:mm');
 }
